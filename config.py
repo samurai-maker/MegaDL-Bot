474981e660a1b6e136f35f7e3209791dacd1cde7
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-10-01 09:20:46 -0700

    Update config.py

--- config.py
+++ config.py
@@ -17,34 +17,37 @@ class Config:
 
 class TEXT:
   ABOUT = """
-🤖 **Name:** {bot_name}
+╔═╗───╔╗───╔╦═╗
+║═╬═╦╦╣╠╦═╦╝║═╣
+╠═║╬║║║═╣╬║╬╠═║
+╚═╣╔╬╗╠╩╩═╩═╩═╝
+──╚╝╚═╝
 
 📝 **Language:** [Python](https://www.python.org)
-
 📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+🤖 **Developer:** [Slogan](https://t.me/DevAXD)
 
-📡 **Hosted On:** [Heroku](https://heroku.com)
 
-🧑‍💻 **Developer:** [Safone](https://t.me/ImSafone)
-
-👥 **Support Group:** [AsmSupport](https://t.me/AsmSupport)
-
-📢 **Updates Channel:** [Ｓ１ ＢＯＴＳ](https://t.me/AsmSafone)
 """
 
   HELP_USER = """
-This is **{bot_name}**
+╔═╗───╔╗───╔╦═╗
+║═╬═╦╦╣╠╦═╦╝║═╣
+╠═║╬║║║═╣╬║╬╠═║
+╚═╣╔╬╗╠╩╩═╩═╩═╝
+──╚╝╚═╝
 
-This Bot Can Download Files & Videos From Mega Links & Upload To Telegram. Just Send Any Mega.nz Link & See The Magic. You Can Also Add or Change Caption: Just Select An Uploaded File/ Video or Forward Me Any Telegram File & Then Write The Text You Want To Be Caption On The File As A Reply To That File & The Text You Wrote Will Be Attached As Caption 😁! 
+**This is**  **{bot_name}**
+**This Bot Can Download Files & Videos From Mega Links & Upload To Telegram. Just Send Any Mega.nz Link & See The Magic. You Can Also Add or Change Caption: Just Select An Uploaded File/ Video or Forward Me Any Telegram File & Then Write The Text You Want To Be Caption On The File As A Reply To That File & The Text You Wrote Will Be Attached As Caption 😁!** 
 
-**Made With ❤️ By @AsmSafone! 👑**
+**Made With ❤️ By @DevAXD! 👑**
 """
 
   START_TEXT = """
 👋🏻 **Hi** {user_mention},
 
-I'm **{bot_name}**
-I Can Download Files & Videos From Mega.nz Links & Upload To Telegram. Please Check Help To Learn More 😉!
+**I'm**  **{bot_name}**
+**I Can Download Files & Videos From Mega.nz Links & Upload To Telegram. Please Check Help To Learn More 😉!****
 
 **Maintained By: {bot_owner}**❤️!
 """
